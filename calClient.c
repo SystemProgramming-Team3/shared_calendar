b31810d2d02b79f3ba477289dbf6e8f962b7ad55
risingstaruj <risingstaruj@gmail.com>
2023-05-25 21:17:38 +0900

    rm mv function added.

--- calClient.c
+++ calClient.c
@@ -12,10 +12,12 @@ int *cc_client_func(char *ip, char *port, char *input);
 int *a_client_func(char *ip, char *port, char *input);
 char *c_client_func(char *ip, char *port, char *input);
 int* rm_client_func(char* ip, char* port, char* input);
+int* mv_client_func(char* ip, char* port, char* input);
 
 
 
 // client_func Function Test용 Stub Code (main function)
+/*
 int main(void)
 {
 	char input[256];
@@ -34,14 +36,15 @@ int main(void)
 	// {
 	// 	printf("%d\n", ret[i]);
 	// }
-	//cret = a_client_func("127.0.0.1", "3001", "a 2023.05.23 you>your");
+	//cret = mv_client_func("127.0.0.1", "3001", "mv 2023.05.25 2023.06.27 you ");
+	//cret = rm_client_func("127.0.0.1", "3001", "rm 2023.05.26");
 
-	//for(i=0;i<31;i++)
-	//{
-	 	//printf("%d\n", cret[i]);
-	//}
-	cdret = c_client_func("127.0.0.1", "3001", "c 2023.05.26 ");
-	puts(cdret);
+	// for(i=0;i<31;i++)
+	// {
+	//  	printf("cret: %d\n", cret[i]);
+	// }
+	//cdret = c_client_func("127.0.0.1", "3001", "c 2023.05.26 ");
+	//puts(cdret);
 	//printf("final:%s", cdret);
 	// char k = 'a';
 	// char *juso = cdret;
@@ -53,6 +56,7 @@ int main(void)
 
 	return 0;
 }
+*/
 
 void read_command_and_send()
 {
@@ -518,6 +522,119 @@ int* rm_client_func(char* ip, char* port, char* input)
 
 	return cal_array;
 }
+
+
+
+int* mv_client_func(char* ip, char* port, char* input)
+{
+	int sock;
+	char message[BUF_SIZE];
+	int str_len;
+	char send_msg[BUF_SIZE];
+	char *temp;
+	char *command = NULL;
+	struct sockaddr_in serv_adr;
+	int add = 0;
+	int del_idx = 0;
+	char comm[3][256];
+	char ret[BUF_SIZE];
+	char *ptr = NULL;
+	char res_array[33][BUF_SIZE];
+	int * cal_array = malloc(sizeof(int)*31);
+		// if(argc!=3) {
+		// printf("Usage : %s <IP> <port>\n", argv[0]);
+		// exit(1);
+		//}
+
+		// input = "Test Test2 Test3";
+		// char test[] = "TEST1 2 3";
+		// char *ptr = strtok(input, " ");
+		// while(ptr!=NULL)
+		//{
+		// strcpy(comm[del_idx], ptr);
+		// printf("%s\n", comm[del_idx]);
+		// ptr = strtok(NULL, " ");
+		//}
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock==-1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(ip);
+	serv_adr.sin_port=htons(atoi(port));
+
+	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+	{
+		//error_handling("connect() error!");
+		cal_array[0] = -1;
+		return cal_array;
+	}
+	else
+		{
+		//puts("Connected......");
+
+
+		strcpy(message, input);
+		//if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			//break;
+		/*
+		if(!strcmp(message, ":help\n"))
+		{
+			fprintf(stdout,"\033[0;31m======LIST OF COMMANDS======\n");
+			fprintf(stdout,"\033[0;34m1. ADD SCHEDULE (a)\n");
+			fprintf(stdout,"2. SHOW SCHEDULE (c)\n");
+			fprintf(stdout, "3. SHOW SCHEDULE OF THE MONTH (cc)\n");
+			fprintf(stdout, "%c[0m\n", 27);
+			continue;
+		}
+		*/
+		if(message[0]=='a')
+		{
+			add=1;
+		}
+
+		//command = strtok(message, " ");
+		//strcpy(temp, command);
+		//printf("%s\n", temp);
+		write(sock, message, strlen(message));
+		str_len=read(sock, message, BUF_SIZE-1);
+		message[str_len]=0;
+
+		printf("%s",message);
+		
+		if(add==1)
+		{
+			
+		//printf("Added Schedule\n");
+		//printf("The day that schedule exists that month: %s\n", message);
+		add=  0;
+		sprintf(ret, "Removed Schedule\nThe day that schedule exists that month: %s\n", message);
+		}
+		else{
+			//printf("The Schedule is: %s \n", message);
+			
+			printf("The Schedule is: %s \n", message);
+			ptr = strtok(message, ",");
+			while(ptr!=NULL)
+			{
+			strcpy(res_array, ptr);
+			//printf("origin: %s\n", ptr);
+			//printf("string: %s\n", res_array[del_idx]);
+			cal_array[del_idx] = atoi(ptr);
+			//printf("d: %d\n", cal_array[del_idx]);
+			ptr = strtok(NULL, ",");
+			del_idx++;
+			}
+		}
+		//printf("%s\n", ret);
+	}
+	close(sock);
+
+
+	return cal_array;
+}
+
 
 
 
